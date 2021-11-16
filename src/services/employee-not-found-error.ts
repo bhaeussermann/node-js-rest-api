e060f59ece9e032bf0d71ca5884ba2272e7c7ae3
Bernhard Häussermann <bernhard.haeussermann@digiata.com>
2021-11-16 09:24:50 +0200

    Revert compiler target to ES5 for better compatibility

--- src/services/employee-not-found-error.ts
+++ src/services/employee-not-found-error.ts
@@ -1,5 +1,7 @@
 export class EmployeeNotFoundError extends Error {
   constructor(employeeId: number) {
     super(`Employee with ID ${employeeId} not found.`);
+    // Restore prototype chain so `instanceof` will work (see https://stackoverflow.com/a/48342359/359765)
+    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
